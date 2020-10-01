472219241c11942279abc611b9cd2b131c760aa9
rtsmit27 <rtsmith27@gmail.com>
2020-10-01 02:13:37 -0500

    last commit before final project

--- app_public/src/app/add-manual-page/add-manual-page.component.ts
+++ app_public/src/app/add-manual-page/add-manual-page.component.ts
@@ -7,44 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddManualPageComponent implements OnInit {
 
-
-  public newRecipe: Recipe = { recipeName: '', instructions: '', ingredients: '' };
-
   public formError: string; 
-
-  private formIsValid(): boolean {
-    if (this.newRecipe.recipeName && this.newRecipe.instructions) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  private resetAndHideRecipeForm(): void {
-    this.newManual.title = '';
-    this.newManual.edition = '';
-    this.newManual.author = '';
-    this.newManual.publisher = '';
-    this.newManual.isbn = '';
-  }
-
-  public onSubmit(): void {
-    this.formError = '';
-    if (this.formIsValid()) {
-      // this.eatrDataService.addRecipeByChefId('5ec30d3a93f206389c58748c',
-      //   this.newRecipe)
-        .then((recipe: Recipe) => {
-          let recipes = this.chef.recipes.slice(0);
-          recipes.unshift(recipe);
-          this.chef.recipes = recipes;
-          this.resetAndHideRecipeForm();
-        });
-    } else {
-      this.formError = 'All fields required, please try again';
-    }
-  }
-
-
 
   constructor() { }
 
